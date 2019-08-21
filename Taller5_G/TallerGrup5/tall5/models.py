f4d66b0235d99e7f77479cf8efbbbcb3955da811
kbermude <42000003+kbermude@users.noreply.github.com>
2019-08-21 10:39:02 -0500

    Update models.py

--- Taller5_G/TallerGrup5/tall5/models.py
+++ Taller5_G/TallerGrup5/tall5/models.py
@@ -10,4 +10,12 @@ class Book(Document):
 	isbn = fields.StringField()
 	rate = fields.StringField()
 
+class User(models.Model):
+	nombre=models.CharField(max_length=60)
+	apellido=models.CharField(max_length=100)
+	cedula=models.CharField(max_length=1)
+	correo=models.CharField(max_length=50)
+	fecha_nacimiento=models.DateField(_("Date"), default=datetime.date.today)
+	
+
 
